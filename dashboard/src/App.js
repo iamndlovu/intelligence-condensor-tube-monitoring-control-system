0f0c82d2c052362d9bfde34b452f9cc963c424e4
Pardon Ndlovu <pndlovu385@icloud.com>
2024-05-20 14:14:07 +0200

    login and register forms

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,11 +1,61 @@
 import { useEffect, useState } from 'react';
 import Dashboard from './components/Dashboard';
+import LoginForm from './components/loginForm/LoginForm';
 
-const dbuser = {
-  fullName: 'John Doe',
-  email: 'john@example.mail',
-  password: '12345',
-};
+const Login = ({ handler }) => (
+  <div
+    style={{
+      display: 'flex',
+      flexDirection: 'column',
+      width: '100%',
+      height: '100vh',
+    }}
+  >
+    <header
+      style={{
+        backgroundColor: 'black',
+        color: 'white',
+        padding: '1rem 2rem',
+        textAlign: 'center',
+      }}
+    >
+      <h1>intelligent condenser monitoring & control system</h1>
+    </header>
+
+    <main
+      style={{
+        flexGrow: 1,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+      }}
+    >
+      <section
+        style={{
+          width: '45rem',
+          margin: 'auto',
+        }}
+      >
+        <LoginForm handler={handler} />
+      </section>
+    </main>
+
+    <footer
+      style={{
+        padding: '1rem 2rem',
+        borderTop: '1px solid #333',
+      }}
+    >
+      <p
+        style={{
+          textAlign: 'center',
+        }}
+      >
+        &copy; {new Date().getFullYear()} CompanyName. All Rights Reserved
+      </p>
+    </footer>
+  </div>
+);
 
 const App = () => {
   const [user, setUser] = useState(null);
@@ -15,8 +65,22 @@ const App = () => {
     setUser(null);
   };
 
+  const loginHandler = (user) => {
+    localStorage.setIItem(
+      'intelligent-condenser-monitoring-system-user',
+      JSON.stringify(user)
+    );
+    setUser(user);
+  };
+
   useEffect(() => {
-    setUser(dbuser);
+    const localUser = localStorage.getItem(
+      'intelligent-condenser-monitoring-system-user'
+    );
+
+    if (localUser) {
+      setUser(JSON.parse(localUser));
+    }
   }, []);
 
   return (
@@ -24,7 +88,7 @@ const App = () => {
       {user ? (
         <Dashboard user={user} logoutHandler={logoutHandler} />
       ) : (
-        'logged out'
+        <Login handler={loginHandler} />
       )}
     </div>
   );
---
+++ dashboard/src/components/registrationForm/RegistrationForm.js
@@ -0,0 +1,120 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+import styles from '../loginForm/LoginForm.module.scss';
+
+const RegistrationForm = ({ handler }) => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirm, setConfirm] = useState('');
+
+  const onChangeFirstName = (e) => setFirstName(e.target.value);
+  const onChangeLastName = (e) => setLastName(e.target.value);
+  const onChangeEmail = (e) => setEmail(e.target.value);
+  const onChangePassword = (e) => setPassword(e.target.value);
+  const onChangeConfirm = (e) => setConfirm(e.target.value);
+
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+
+    if (password !== confirm) {
+      alert('Passwords do not match');
+      return;
+    }
+    const fullName = `${firstName} ${lastName}`;
+    const newUser = { email, password, fullName };
+    axios
+      .post('http://localhost:5000/users/add', newUser)
+      .then((res) => res.data)
+      .then((data) => console.log(data))
+      .then(() => alert('User added'))
+      .then(() => handler())
+      .catch((error) => alert(error));
+  };
+
+  return (
+    <form
+      className={styles.LoginForm}
+      onSubmit={onSubmitForm}
+      style={{
+        margin: '1.3rem 0',
+      }}
+    >
+      <div className={styles.formGroup}>
+        <label htmlFor='firstname' className={styles.offscreen}>
+          Your First Name
+        </label>
+        <input
+          type='text'
+          name='firstname'
+          id='firstname'
+          placeholder='First Name'
+          value={firstName}
+          onChange={onChangeFirstName}
+          required
+        />
+      </div>
+      <div className={styles.formGroup}>
+        <label htmlFor='lastname' className={styles.offscreen}>
+          Your Last Name(s)
+        </label>
+        <input
+          type='text'
+          name='lastname'
+          id='lastname'
+          placeholder='Last Name(s)'
+          value={lastName}
+          onChange={onChangeLastName}
+          required
+        />
+      </div>
+      <div className={styles.formGroup}>
+        <label htmlFor='email' className={styles.offscreen}>
+          Your email address
+        </label>
+        <input
+          type='email'
+          name='email'
+          id='email'
+          placeholder='Email'
+          value={email}
+          onChange={onChangeEmail}
+          required
+        />
+      </div>
+      <div className={styles.formGroup}>
+        <label htmlFor='password' className={styles.offscreen}>
+          Your password
+        </label>
+        <input
+          type='password'
+          name='password'
+          id='password'
+          placeholder='Password'
+          value={password}
+          onChange={onChangePassword}
+          required
+        />
+      </div>
+      <div className={styles.formGroup}>
+        <label htmlFor='confirm' className={styles.offscreen}>
+          Confirm password
+        </label>
+        <input
+          type='password'
+          name='confirm'
+          id='confirm'
+          placeholder='Confirm Password'
+          value={confirm}
+          onChange={onChangeConfirm}
+          required
+        />
+      </div>
+      <input type='submit' value='Submit' />
+    </form>
+  );
+};
+
+export default RegistrationForm;
